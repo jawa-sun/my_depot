492f88e39c46a9842638927e59050762987cccef
jawa-sun <3356477688@qq.com>
2024-11-23 01:20:26 +0800

    Add files via upload

---
+++ 练习1(1)/练习1.js
@@ -0,0 +1,10 @@
+function f(){
+  const time=document.getElementById("time-display");
+  const now =new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  time.textContent = hours + ":" + minutes + ":" + seconds
+}
+f()
+setInterval(f,1000)
